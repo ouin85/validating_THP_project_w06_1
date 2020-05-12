fd99975efd5ef9902ea89416d7d583ed6e2ee69a
ouin85 <bouinten@yahoo.fr>
2020-05-12 07:25:46 +0200

    add events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Root route
+  root 'events#index'
+  
+  # Set events_contrller routes
+  resources :events
 end
