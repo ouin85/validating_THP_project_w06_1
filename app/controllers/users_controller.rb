f5f5c4df5176f3d060b76d20830ed59646d617c6
ouin85 <bouinten@yahoo.fr>
2020-05-12 23:15:48 +0200

    ---

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:info] = 'User seccessfully updated'
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
   
   private
   def is_current_user_showing?
@@ -12,5 +26,9 @@ class UsersController < ApplicationController
       flash[:danger] = "You can't display a profile that is not yours !"
       redirect_to root_path
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
   end
 end
